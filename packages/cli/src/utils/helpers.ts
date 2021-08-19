8df707006c4d21535b9c31f7c7ebe07d1d49ee82
Cristian Leyes <cristiandley@users.noreply.github.com>
2021-08-19 17:28:30 -0300

    fix(all): :art: wording, unnused files, cli build (#6)
    * fix(all): :art: wording, unnused files, cli build
    
    * fix(all): :art: wording, unnused files, cli build
    
    * chore(release): publish
    
     - @securityscorecard/cli-manifest@1.0.6
     - @securityscorecard/cli@0.2.0
     - @securityscorecard/sdk@0.2.0
    
    Co-authored-by: cristiandley <cleyes@securityscorecard.io>

--- packages/cli/src/utils/helpers.ts
+++ packages/cli/src/utils/helpers.ts
@@ -5,7 +5,13 @@ export const SSC_RC_PATH: string = `${os.homedir()}/.sscrc`;
 export const ENVIROMENTS: string[] = ['production', 'testing', 'development'];
 
 export const API: { [key: string]: string } = {
-  production: '',
-  testing: '',
+  production: 'https://platform-api.securityscorecard.io',
+  testing: 'https://platform-api.securityscorecard.qa',
   development: 'https://platform-api.securityscorecard.tech',
+};
+
+export const PLATFORM_URL: { [key: string]: string } = {
+  production: 'https://platform.securityscorecard.io/#i',
+  testing: 'https://platform.securityscorecard.qa/#',
+  development: 'https://platform.securityscorecard.tech/#',
 };
